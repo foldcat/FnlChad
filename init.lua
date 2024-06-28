76be2e0ffc7149d80bb3ed1c3783ee2e7da8343e
foldcat <151979723+foldcat@users.noreply.github.com>
2024-06-28 14:01:04 +0800

    fix line number disappearing

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+vim.wo.number = true
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
