b40db673c6bfe098464adb424b8c30e4fe60bb7a
foldcat <151979723+foldcat@users.noreply.github.com>
2024-07-03 15:59:55 +0800

    delete unneeded code

--- init.lua
+++ init.lua
@@ -4,11 +4,6 @@ vim.g.maplocalleader = ","
 
 vim.wo.number = true
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
-vim.g.mapleader = " "
-
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
